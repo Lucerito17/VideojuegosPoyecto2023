8bb93bf87c527d4b3c6a60edac91126bd42942a0
Lucerito <51010556+Lucerito17@users.noreply.github.com>
2023-06-26 15:45:26 -0500

    cinco

---
+++ ProyectoFinal/Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public void NivelPrueba()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
